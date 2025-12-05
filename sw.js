1e1980cb7e84bff44b29a2dfba9e5b0402d41900
Félix Meyrant <felix@connexion.agency>
2025-12-05 14:49:18 +0100

    V2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SPOT THE SUN - Service Worker
 // ==========================================
 
-const CACHE_NAME = 'spot-the-sun-v1';
+const CACHE_NAME = 'spot-the-sun-v4';
 const OFFLINE_URL = '/offline.html';
 
 // Files to cache for offline use
